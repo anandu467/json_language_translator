62c344622a9399559c40b7498652561a66a5f049
Anandu M <you@example.com>
2021-05-02 11:19:10 +0530

    Code Refactoring and Optimisations

--- lib/translator.py
+++ lib/translator.py
@@ -1,59 +1,67 @@
 import json
 from deep_translator import GoogleTranslator
 
-report = "\n\nTranslation Report \n --------------------"
 translationCount=0
 
-
 def translate(data,language):
-    global report
-    global translationCount
+
     if not (data): return data
-    if(type(data) in  [int,float]):
-        return data
-    if type(data) ==list:
-        return [translate(x,language) for x in data ]
-    for x in data:
-        if(type(data[x]) == str and data[x].strip() !=""):
-            print("-> "+data[x])
-            try:
-                data[x] = GoogleTranslator(
-                    source='en', target=language).translate(data[x])
-                translationCount+=1
-            except:
-                report += data[x]+" : not translated \n"
-        else:
-            data[x] = translate(data[x],language)
-    return data
+    dataType=type(data)
+    translator=getTranslator(dataType)
+    return translator(data,language)
 
 
-# with open(fileName) as inputFile:
-#     with open(fileName.split(".")[0]+"_translated.json", "w", encoding="utf-8") as outputFile:
-#         data = json.load(inputFile)
-#         json.dump(translate(data), outputFile, indent=4, ensure_ascii=False)
-#         report+="\n"+str(translationCount)+" records translated"
-#         print(report)
+def getTranslator(dataType):
+    tranlatorMap={
+        int:translateInt,
+        float:translateInt,
+        list:translateList,
+        str:translateWord,
+        dict:translateDict
+    }
+    return tranlatorMap[dataType]
 
+def translateWord(word,language):
+    if(not shouldTranslate(word)):
+        return word
+    try:
+        logTranslation(word)
+        word =GoogleTranslator(
+                    source='en', target=language).translate(word)
+        incrementTranslationCount()
+    except:
+        print("Error translating ",word)
+        pass
+    return word
+def translateList(wordList,language):
+    return [translate(x,language) for x in wordList]
+def translateInt(word,language):
+    return word
+def translateDict(wordDict,language):
+        for key in wordDict:
+            value = wordDict[key]
 
+            wordDict[key] = translate(value,language)
+        return wordDict
+
+def shouldTranslate(word):
+    return type(word) == str and word.strip() !="" and not word.isdigit()
 def validateJSON(inputJson):
     try:
         json.loads(inputJson)
         return "valid"
     except :
         return "invalid_json"
-    
-
+def logTranslation(word):
+    print("-> ",word)    
+def incrementTranslationCount():
+    global translationCount;
+    translationCount +=1
 def start_translate(inputText,language,window):
             print("Translation started..")
             global translationCount;
             translationCount=0
-
-
-
-
-            
             data = json.loads(inputText)
-
             data=translate(data,language)
             print(str(translationCount)+" records translated")
             window.write_event_value("-TRANSLATED-",json.dumps(data,indent=4, ensure_ascii=False))
